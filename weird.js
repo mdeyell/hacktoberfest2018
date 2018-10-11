721e2dafc997427abb694102b4bfaad8cac43b27
Matthew Deyell <mdeyell@ucsc.du>
2018-10-10 21:00:12 -0700

    added weird javascript stuff
    https://github.com/denysdovhan/wtfjs#-examples

---
+++ weird.js
@@ -0,0 +1,25 @@
+class weird {
+   /*
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }*/
+  weirdStuff(){
+     var  i = Number.MIN_VALUE
+     if ((i * i) == 0){
+       console.log('WHY!!!!111!!');
+     }
+     if ((i +1) == 1){
+      console.log('WHY!!!!111!!');
+     }
+     if ((i - 1) == -1){
+      console.log('WHY!!!!111!!');
+     }
+     if ((i /i) == 1){
+       console.log('WHY!!!!111!!');
+     }
+  }
+}
+module.exports = weird;
+
+
